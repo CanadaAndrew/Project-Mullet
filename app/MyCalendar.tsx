f5bcdcacdf69ef96f21b5b4baa75a8060ced30ad
acovert2 <112994979+acovert2@users.noreply.github.com>
2024-01-24 22:18:38 -0800

    DT-134 Calendar getter method added
    Updated calendar to add getter function. Didn't add a setter function as it already sets dates when selected by user.

--- app/MyCalendar.tsx
+++ app/MyCalendar.tsx
@@ -27,6 +27,12 @@ const MyCalendar: React.FC<MyCalendarProps> = ({ pageName, disabled = false }) =
     handleDatesSelected(newSelectedDates, pageName);
   };
 
+  /* Getter to allow other pages to grab dates from calendar
+  Example: const selectedDates = calendarContainerRef.current.getSelectedDates();*/
+  const getSelectedDates = () => {
+    return selectedDates;
+  };
+
   return (
     <View style={styles.container}>
       <Calendar
