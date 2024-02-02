cefd4f5cc32c1043d2d51c755546e736c80030eb
ttang0 <65149380+ttang0@users.noreply.github.com>
2024-02-01 23:25:38 -0800

    Started making the aboutme page

---
+++ app/AboutMe.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View, Pressable, FlatList} from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import { Calendar } from 'react-native-calendars';
+import { Link } from 'expo-router';
+import { SelectList } from 'react-native-dropdown-select-list';
+import MyCalendar from './MyCalendar';
+
+export default function AboutMe() {
+    return (
+    <Text>kldjas;fj;lasdkfjl;kads</Text>
+)}
+
