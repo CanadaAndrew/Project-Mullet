66207daa9d4ba4fef9fc6287698c42822b97f988
CanadaAndrew <43078625+CanadaAndrew@users.noreply.github.com>
2024-02-06 16:16:57 -0800

    Fixing merge errors

--- app/NavigateHome.tsx
+++ app/NavigateHome.tsx
@@ -31,6 +31,7 @@ export default function NavigateHome(){
             <Button
                 title="Client History page"
                 onPress={() => navigation.navigate("ClientHistory")}
+            />
             <Button  
                 title = "AboutMe page"
                 onPress={() => navigation.navigate("AboutMe", {
