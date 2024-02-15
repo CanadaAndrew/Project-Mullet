9d13c1c452147ef8f97e40474bcfac0e3e745c0f
wilsontoy <wilsontoy1@gmail.com>
2024-02-14 17:01:24 -0800

    DT-31 and DT-32: Elements and Styling for Sign Up Screen

--- app/index.tsx
+++ app/index.tsx
@@ -14,6 +14,7 @@ import HomeScreen from './HomeScreen';
 import appointmentsClientView from './appointmentsClientView';
 import ClientHistory from './ClientHistory';
 import AboutMe from './AboutMe';
+import SignUp from './SignUp';
 
 const Stack = createNativeStackNavigator()
 
@@ -66,6 +67,11 @@ export default function index() {
          <Stack.Screen name = "ServicesOffered" component={ServicesOffered} />
          <Stack.Screen name = "ClientHistory" component={ClientHistory} />
          <Stack.Screen name = "AboutMe" component={AboutMe}/>
+        <Stack.Screen name="SignUp" component={SignUp}
+            options={{
+                title: "Sign Up"
+            }}
+        />
       </Stack.Navigator>
    </NavigationContainer>
   
