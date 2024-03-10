88b663020a999f0633236d34f71318221d8d189b
CanadaAndrew <43078625+CanadaAndrew@users.noreply.github.com>
2024-03-09 21:30:26 -0800

    Merge fix

--- app/NavigateHome.tsx
+++ app/NavigateHome.tsx
@@ -83,7 +83,7 @@ export default function NavigateHome(){
             <Button
                 title = "Sign Up page"
                 onPress = {() => navigation.navigate("SignUp", {userData})}
-            
+            />
             
         </View>
     )
